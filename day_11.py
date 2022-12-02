06295a3ed55fd1275bc229257bd7ee7dfd4b120f
Aberratio Cultura Visual <57122156+1Aberratio@users.noreply.github.com>
2022-12-02 21:42:27 +0000

    Create day_11.py

---
+++ day_11.py
@@ -0,0 +1,20 @@
+# Day 11: Are They Equal?
+
+def equal_strings(str1, str2):    
+    if sorted(str1) == sorted(str2): return True
+    else: return False
+
+word_a = 'live'
+word_b = 'evil'
+print(equal_strings(word_a, word_b))
+
+print('--------------------------')
+
+# Extra Challenge: Swap Values
+
+def swap_values(numbers):
+    numbers[0], numbers[-1] = numbers[-1], numbers[0]
+    return numbers
+
+numbersList = [2, 4, 67, 7]
+print(swap_values(numbersList))
